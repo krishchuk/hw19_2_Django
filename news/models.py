9a6de5c5d0d843b2313370b0872599550b5b3999
krishchuk <ms.den.best@mail.ru>
2024-03-24 02:36:26 +0500

    fix displaying pictures

--- news/models.py
+++ news/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='заголовок')
     slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
     content = models.TextField(verbose_name='содержимое')
-    picture = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    picture = models.ImageField(upload_to='news/', verbose_name='изображение', **NULLABLE)
     created_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
     is_published = models.BooleanField(default=False, verbose_name='опубликлвано')
     views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
