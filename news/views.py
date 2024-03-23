ed041e4fbea36d497aa0d039694d9a38b8bf3ea8
krishchuk <ms.den.best@mail.ru>
2024-03-24 00:56:01 +0500

    hw_21.1_part3

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+from pytils.translit import slugify
 
 from news.models import News
 
@@ -10,6 +12,11 @@ class NewsListView(ListView):
     extra_context = {
         'title': "Новости"
     }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_published=True)
+        return queryset
 
 
 class NewsDetailView(DetailView):
@@ -23,23 +30,39 @@ class NewsDetailView(DetailView):
 
         return context_data
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class NewsCreateView(CreateView):
     model = News
-    fields = ('title', 'content', 'picture', 'slug', 'is_published',)
+    fields = ('title', 'content', 'picture', 'is_published',)
     success_url = reverse_lazy('news:news_list')
     extra_context = {
         'title': "Добавить новость"
     }
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+        return super().form_valid(form)
+
 
 class NewsUpdateView(UpdateView):
     model = News
-    fields = ('title', 'content', 'picture', 'slug', 'is_published',)
+    fields = ('title', 'content', 'picture', 'is_published',)
     success_url = reverse_lazy('news:news_list')
     extra_context = {
         'title': "Изменить новость"
     }
+
+    def get_success_url(self):
+        return reverse('news:news_detail', args=[self.kwargs.get('pk')])
 
 
 class NewsDeleteView(DeleteView):
