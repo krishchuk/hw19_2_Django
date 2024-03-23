7535a1bca609e56f15710859e27775e06e8dcdce
krishchuk <ms.den.best@mail.ru>
2024-03-24 00:21:18 +0500

    delete def contacts from views.py

--- catalog/views.py
+++ catalog/views.py
@@ -15,37 +15,6 @@ class HomeListView(ListView):
     }
 
 
-# def contacts(request):
-#     context = {
-#         'title': "Контакты"
-#     }
-#     # Получение данных от пользователя и их сохранение в JSON
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         user_contacts = {
-#             'name': name,
-#             'phone': phone,
-#             'message': message
-#         }
-#         # Запись в уже существующий файл с данными
-#         if os.path.isfile('data.json'):
-#             with open('data.json', 'r') as file:
-#                 user_data: list[dict] = json.load(file)
-#                 user_data.append(user_contacts)
-#             with open('data.json', 'w') as file:
-#                 json.dump(user_data, file, indent=4)
-#             # Вывод JSON в терминал
-#             print(user_data)
-#         # Создание файла и первая запись данных
-#         else:
-#             with open('data.json', 'w') as file:
-#                 json.dump([user_contacts], file, indent=4)
-#
-#     return render(request, 'catalog/contacts.html', context)
-
-
 class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
     extra_context = {
